67d16f8f98a8170d752cde169155c3f75654915d
armalik <armalik@ebay.com>
2020-11-30 13:55:08 +0200

    Add printenv test

--- src/test/java/com/jenkinsexample/jenkinsdemo/JenkinsdemoApplicationTests.java
+++ src/test/java/com/jenkinsexample/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -3,11 +3,21 @@ package com.jenkinsexample.jenkinsdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
+
 @SpringBootTest
 class JenkinsdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void printenv() {
+		for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+
 	}
 
 }
